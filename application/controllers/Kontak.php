fc9063df08c0cea3a0e0c84647c444d0b503c787
unknown <reyhan.a.deris@gmail.com>
2023-07-28 10:51:51 +0700

    Menambah fitur kelola data kontak dan menghapus assets yang berantakan

---
+++ application/controllers/Kontak.php
@@ -0,0 +1,49 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Kontak extends CI_Controller {
+    public function __construct(){
+        parent::__construct();
+        $this->load->helper('url'); 
+        $this->load->model('KontakModel');
+        $this->load->library('session');
+        if (!$this->session->userdata('logged_in')) {
+            redirect('Admin/loginPage');
+        }
+    }
+    
+    public function tampilKontak(){
+        $data['active_menu'] = 'data_kontak';
+        $data['data_kontak']= $this->KontakModel->getKontak();
+        $this->load->view("admin/data-kontak", $data);
+        $this->load->view("admin/sidebar", $data);
+
+    }
+
+    public function editKontak($id_kontak){
+        $data['active_menu'] = 'data_kontak';
+        $data['data_kontak']= $this->KontakModel->getKontakById($id_kontak);
+        $this->load->view("admin/edit-kontak", $data);
+        $this->load->view("admin/sidebar", $data);
+
+    }
+
+    public function update($id_kontak){
+        $id_kontak = $this->input->post('id_kontak');
+        $alamat = $this->input->post('alamat');
+        $phone = $this->input->post('phone');
+        $email = $this->input->post('email');
+        $maps = $this->input->post('maps');
+
+        // Mengupdate data kontak
+        $data = array(
+            'id_kontak' => $id_kontak,
+            'alamat' => $alamat,
+            'phone' => $phone,
+            'email' => $email,
+            'maps' => $maps
+        );
+        $this->KontakModel->updateKontak($id_kontak, $data);
+        redirect('Kontak/tampilKontak');
+    }
+}
